6c9f9fe3b0000421a0fb10a6415e1d4a6cb9d543
GitDogBar <Dogbar>
2021-08-15 23:37:07 +0700

    Add project files.

---
+++ Month.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum Month
+    { //จริงๆแล้ว index ของ enum เริ่มต้นจาก 0 เหมือนใน array
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        Decenber = 12
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Month thisMonth = Month.August;
+
+            int showIndexThisMonth = (int)thisMonth;
+
+            Console.WriteLine(showIndexThisMonth);//แสดงผลออกมาเป็นตัวเลขของเดือน August
+
+            Console.WriteLine((int)Month.Decenber);//แสดงผลออกมาเป็นตัวเลขของเดือน
+
+            Month strThisMonth = (Month)2;
+
+            Console.WriteLine(strThisMonth);
+        }
+    }
+}
